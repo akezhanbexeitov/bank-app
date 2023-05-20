0c8d4d8629d3b778a1950874f985fa32440837f6
Akezhan <akidulatini@gmail.com>
2023-05-20 23:03:51 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...">
+        Save changes
+      </button>
+    </>
+  )
+}
+
+export default App
